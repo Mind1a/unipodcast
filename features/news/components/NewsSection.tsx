79050e6a1a4f0380355ba4d68cfb3bc08e8f063b
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2026-08-10 15:05:39 +0400

    Add cursor-pointer to news section button
    Adds cursor-pointer class to the button element for improved user experience, making the clickable button state more obvious to users.

--- features/news/components/NewsSection.tsx
+++ features/news/components/NewsSection.tsx
@@ -27,7 +27,7 @@ const NewsSection = () => {
         </div>
       </div>
       <Link href={"/about"} className="max-w-81.5 w-full mx-auto group ">
-        <button className="w-full border-[0.5px] border-[#FED403] py-3 pr-3 pl-3.5 flex bg-black group-hover:bg-[#F3CD11] rounded-4xl justify-end gap-[39px] transition-colors duration-300">
+        <button className="w-full border-[0.5px] border-[#FED403] py-3 pr-3 pl-3.5 flex bg-black group-hover:bg-[#F3CD11] rounded-4xl justify-end gap-[39px] transition-colors duration-300 cursor-pointer">
           <span className="text-[#FED403] group-hover:text-black font-semibold text-base md:text-[18px] h-5.5 ease-in-out  transition-colors duration-300">
             ნახე ყველა სიახლე
           </span>
